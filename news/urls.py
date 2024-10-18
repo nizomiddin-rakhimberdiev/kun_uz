cbfe9278658fc8eb29137518faa9f7c89f5d893d
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2024-10-18 16:55:46 +0500

    add categories and main page

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from news.views import index
+from news.views import index, categories_view, category_detail
 
 urlpatterns = [
     path('', index, name='home'),
+    path('categories/', categories_view, name='categories_view'),
+    path('categories/<int:id>/', category_detail, name='category_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,15 +1,47 @@
+from dis import CACHE
+
 from django.shortcuts import render
 
-from news.models import News
+from news.models import News, Category
 
 
 # Create your views here.
 
 def index(request):
-    news = News.objects.order_by('-published_time')
+    news = News.objects.order_by('-published_time')[0:5]
+    latest_news = News.objects.order_by('-published_time')[0]
+    latest_three_news = News.objects.order_by('-published_time')[1:4]
     context = {
         'news': news,
+        'latest_news': latest_news,
+        'latest_three_news': latest_three_news,
     }
     return render(request, 'index.html', context)
 
+
+def categories_view(request):
+    categories = Category.objects.all()
+    news = News.objects.all()
+    context = {
+        'categories': categories,
+        'news': news,
+    }
+    return render(request, 'categori.html', context)
+
+
+def category_detail(request, id):
+    categories = Category.objects.all()
+    news = News.objects.filter(category__id=id)
+    context = {
+        'categories': categories,
+        'news': news,
+    }
+    return render(request, 'categori.html', context)
+
+def nimadir(request, id):
+    news = News.objects.get(id=id)
+    context = {
+        'news': news,
+    }
+    return render(request, 'nimadir.html', context)
 
